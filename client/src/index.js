6b64d388db106e4c797a6612798472eb2991fbc0
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-09-06 15:58:34 -0400

    im so confused

--- client/src/index.js
+++ client/src/index.js
@@ -13,55 +13,55 @@ ReactDOM.render(
 );
 
 
-//   state = {
-//     data: [],
-//     dataText: ''
-//   }
+  state = {
+    data: [],
+    dataText: ''
+  }
 
-//   componentDidMount() {
-//     fetch('https://googletrends.github.io/data/')
-//       .then(res => res.json())
-//       .then(res => this.setState({data: res.message }));
+  componentDidMount() {
+    fetch('https://googletrends.github.io/data/')
+      .then(res => res.json())
+      .then(res => this.setState({data: res.message }));
 
 
-//   handleChange = event => {
-//     this.setState({
-//       dataText: event.target.value
-//     });
-//   };
+  handleChange = event => {
+    this.setState({
+      dataText: event.target.value
+    });
+  };
 
-//   fetchData = e => {
-//   fetch('https://googletrends.github.io/data/')
-//       .then(res => res.json())
-//       .then(res => this.setState({data: res.message }))
-//       .catch(err => console.log('noo'));
-// }
+  fetchData = e => {
+  fetch('https://googletrends.github.io/data/')
+      .then(res => res.json())
+      .then(res => this.setState({data: res.message }))
+      .catch(err => console.log('noo'));
+}
 
-//   render() {
-//   return (
-//     <div className="App">
-//       <h1>Hello Data</h1>
-//       <input 
-//       type="text" 
-//       value={this.state.dataText}
-//       onChange={this.handleChange}
-//       />
+  render() {
+  return (
+    <div className="App">
+      <h1>Hello Data</h1>
+      <input 
+      type="text" 
+      value={this.state.dataText}
+      onChange={this.handleChange}
+      />
 
-//       <button onClick={this.fetchData}>Fetch Data</button>
-//       <div className='data'>
-//       {this.state.data.map(data => (
-//         <img width='200' src={data} key={data} alt={data} />
-//       ))}
-//       </div>
-//     </div>
-//   );
-// }
-// }
+      <button onClick={this.fetchData}>Fetch Data</button>
+      <div className='data'>
+      {this.state.data.map(data => (
+        <img width='200' src={data} key={data} alt={data} />
+      ))}
+      </div>
+    </div>
+  );
+}
+}
 
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
-// // .then(res => res.json())
-// //       .then(dogs => this.setState({ doggos: dogs.message}))
-// //       .catch(err => console.log('nooo'));
-// //   
+// .then(res => res.json())
+//       .then(dogs => this.setState({ doggos: dogs.message}))
+//       .catch(err => console.log('nooo'));
+//   
