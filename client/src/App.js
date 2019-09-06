e57ea1733718f487d9cf305ad2517e1ee04c4cda
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-09-06 15:50:45 -0400

    mes

--- client/src/App.js
+++ client/src/App.js
@@ -8,22 +8,9 @@ import 'semantic-ui-css/semantic.min.css'
 
  export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+     <PlayerList />
+   </main>
   );
 }
 
